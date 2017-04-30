e9fc31181d1d9f768d51e84097a7cc16306b45df
Natty Davis <natty.davis79@gmail.com>
2017-04-30 15:09:36 -0400

    all is well

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(bodyParser.json());
 
 
 //use our routes
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/signup', signUpRouter);
 app.use('/login', loginRouter)
 
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React    from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(<div>Hello world from react and react dom</div>, document.querySelector('#root-container'));
